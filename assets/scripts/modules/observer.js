c20b8c70105b807ff38f1c567ddb594696e3f5e2
mia <mia_charlotte.henrichsmeyer@smail.th-koeln.de>
2024-05-08 12:36:17 +0200

    Observer hinzugefügt

--- assets/scripts/modules/observer.js
+++ assets/scripts/modules/observer.js
@@ -2,9 +2,24 @@
 ---------------------------------------------------------------------------- */
 
 const addObserver = () => {
+    const observedElements = document.querySelectorAll('[data-js-observe]');
+    const options = { margin: '0px', threshold: 0.6 };
 
-}
+    const observer = new IntersectionObserver (function (entries, observer) {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("is-in-view");
+            } else {
+                entry.target.classList.remove("is-in-view");
+            }
+        });
+    });
 
+    observedElements.forEach(observedElements => {
+        observer.observe(observedElements);
+    });
+
+};
 
 /* Exports
 ############################################################################ */
